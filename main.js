2552fabb912e3b2a85f5f87584b3e00dea1941a1
miyawang <miyawang1019@gmail.com>
2018-06-23 20:25:40 +0800

    优化

--- main.js
+++ main.js
@@ -40,26 +40,3 @@ function makeLeave($node){
 function makeEnter($node){
   $node.removeClass('current leave').addClass('enter')
 }
-// setTimeout(()=>{
-//   $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
-//   .one('transitionend',(e)=>{
-//     $(e.currentTarget).removeClass('leave').addClass('enter')
-//   })
-//   $('.images > img:nth-child(3)').removeClass('enter').addClass('current');
-// },6000)
-
-// setTimeout(()=>{
-//   $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
-//   .one('transitionend',(e)=>{
-//     $(e.currentTarget).removeClass('leave').addClass('enter')
-//   })
-//   $('.images > img:nth-child(1)').removeClass('enter').addClass('current');
-// },9000)
-
-// setTimeout(()=>{
-//   $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
-//   .one('transitionend',(e)=>{
-//     $(e.currentTarget).removeClass('leave').addClass('enter')
-//   })
-//   $('.images > img:nth-child(2)').removeClass('enter').addClass('current');
-// },12000)
